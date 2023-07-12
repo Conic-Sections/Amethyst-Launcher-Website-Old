eaaff155a5eaaad94bba5d398461fdc3adc5a4d0
Broken Deer <kxscluabcbook@outlook.com>
2023-07-12 13:42:23 +0800

    GitHub webhook test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
+/*
+ * Magical Launcher Core
+ * Copyright (C) 2023 Broken-Deer <old_driver__@outlook.com> and contributors
+ *
+ * This program is free software, you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #[macro_use]
 extern crate rocket;
 
-use std::path::{Path, PathBuf};
-
-use rocket::fs::NamedFile;
-use rocket_dyn_templates::{context, Template};
+use rocket_dyn_templates::Template;
+use router::{
+    api::github_api,
+    web::{assets, download_page, index, update_page},
+};
 
 pub mod app;
 pub mod router;
@@ -12,17 +31,9 @@ pub mod router;
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, assets])
+        .mount(
+            "/",
+            routes![index, assets, download_page, update_page, github_api],
+        )
         .attach(Template::fairing())
-}
-
-#[get("/assets/<file..>")]
-pub async fn assets(file: PathBuf) -> Option<NamedFile> {
-    println!("file: {:?}", file);
-    NamedFile::open(Path::new("public/assets").to_path_buf().join(file)).await.ok()
-}
-
-#[get("/")]
-pub async fn index() -> Template {
-    Template::render("index", context! {foo: 123})
 }
---
+++ src/router/web.rs
@@ -0,0 +1,46 @@
+/*
+ * Magical Launcher Core
+ * Copyright (C) 2023 Broken-Deer <old_driver__@outlook.com> and contributors
+ *
+ * This program is free software, you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use std::path::{Path, PathBuf};
+
+use rocket::fs::NamedFile;
+use rocket_dyn_templates::{context, Template};
+
+#[get("/assets/<file..>")]
+pub async fn assets(file: PathBuf) -> Option<NamedFile> {
+    println!("file: {:?}", file);
+    NamedFile::open(Path::new("public/assets").to_path_buf().join(file))
+        .await
+        .ok()
+}
+
+#[get("/")]
+pub async fn index() -> Template {
+    Template::render("index", context! {foo: 123})
+}
+
+#[get("/download")]
+pub async fn download_page() -> Template {
+    Template::render("download", context! {})
+}
+
+#[get("/update")]
+pub async fn update_page() -> Template {
+    Template::render("update", context! {})
+}
+
